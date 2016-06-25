fd1ecfaf8dba0f32a0e0f24fb840eb77acaf7e80
romainmenke <romainmenke@gmail.com>
2016-06-25 12:43:04 +0200

    universal-notifier client

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"log"
+
+	"limbo.services/trace"
+
+	"github.com/romainmenke/universal-notifier/pkg/wercker"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	span, _ := trace.New(ctx, "New Send Wercker Message Job")
+	defer span.Close()
+
+	env, err := wercker.New(ctx)
+
+	if err != nil {
+		return
+	}
+
+	message, err := env.NewMessage(ctx)
+
+	conn, err := grpc.Dial(env.Host(), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := wercker.NewNotificationServiceClient(conn)
+	_, err = c.Notify(ctx, message)
+	if err != nil {
+		span.Error(err)
+	}
+
+}
--- pkg/wercker/wercker-env.go
+++ pkg/wercker/wercker-env.go
@@ -1,17 +1,151 @@
 package wercker
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	"golang.org/x/net/context"
 	"limbo.services/trace"
 )
 
 type WerckerEnv struct {
+	port string
+	host string
+
+	started        string
+	ci             string
+	buildURL       string
+	domain         string
+	owner          string
+	repo           string
+	branch         string
+	commit         string
+	root           string
+	source         string
+	cache          string
+	output         string
+	user           string
+	applicationURL string
+	result         string
+	stepName       string
+	stepMessage    string
+	action         Build_Action
 }
 
-func New(ctx context.Context) *WerckerEnv {
+func New(ctx context.Context) (*WerckerEnv, error) {
 
 	span, _ := trace.New(ctx, "Send batch of messages to Index SQS Queue")
 	defer span.Close()
 
-	return nil
+	env := WerckerEnv{}
+
+	env.port = os.Getenv("REMOTE_PORT")
+	env.host = os.Getenv("REMOTE_HOST")
+
+	if env.port == "" || env.host == "" {
+		return nil, span.Error("Set REMOTE_HOST and REMOTE_PORT")
+	}
+
+	env.started = os.Getenv("WERCKER_MAIN_PIPELINE_STARTED")
+
+	env.ci = os.Getenv("CI")
+	env.buildURL = os.Getenv("WERCKER_RUN_URL")
+
+	env.domain = os.Getenv("WERCKER_GIT_DOMAIN")
+	env.owner = os.Getenv("WERCKER_GIT_OWNER")
+	env.repo = os.Getenv("WERCKER_GIT_REPOSITORY")
+	env.branch = os.Getenv("WERCKER_GIT_BRANCH")
+	env.commit = os.Getenv("WERCKER_GIT_COMMIT")
+
+	env.root = os.Getenv("WERCKER_ROOT")
+	env.source = os.Getenv("WERCKER_SOURCE_DIR")
+	env.cache = os.Getenv("WERCKER_CACHE_DIR")
+	env.output = os.Getenv("WERCKER_OUTPUT_DIR")
+
+	env.user = os.Getenv("WERCKER_STARTED_BY")
+	env.applicationURL = os.Getenv("WERCKER_APPLICATION_URL")
+
+	env.result = os.Getenv("WERCKER_RESULT")
+	env.stepName = os.Getenv("WERCKER_FAILED_STEP_DISPLAY_NAME")
+	env.stepMessage = os.Getenv("WERCKER_FAILED_STEP_MESSAGE")
+
+	action := os.Getenv("DEPLOY")
+
+	fmt.Println(action)
+	if action == "" {
+		env.action = Build_DEPLOY
+	} else {
+		env.action = Build_BUILD
+	}
+
+	return &env, nil
+}
+
+func (e *WerckerEnv) NewGit() *Git {
+	return &Git{
+		Domain:     e.domain,
+		Owner:      e.owner,
+		Repository: e.repo,
+		Branch:     e.branch,
+		Commit:     e.commit,
+	}
+}
+
+func (e *WerckerEnv) NewResult() *Result {
+	var result bool
+	if e.result == "failed" {
+		result = false
+	} else {
+		result = true
+	}
+
+	return &Result{
+		Result:            result,
+		FailedStepName:    e.stepName,
+		FailedStepMessage: e.stepMessage,
+	}
+}
+
+func (e *WerckerEnv) NewBuild(ctx context.Context) (*Build, error) {
+
+	span, _ := trace.New(ctx, "Create Build Struct")
+	defer span.Close()
+
+	started, err := strconv.ParseInt(e.started, 10, 64)
+	if err != nil {
+		return nil, span.Error(err)
+	}
+
+	return &Build{
+		Started: started,
+		Url:     e.buildURL,
+		User:    e.user,
+		Action:  e.action,
+	}, nil
+}
+
+func (e *WerckerEnv) NewMessage(ctx context.Context) (*WerckerMessage, error) {
+
+	span, _ := trace.New(ctx, "Create Wercker Message")
+	defer span.Close()
+
+	build, err := e.NewBuild(ctx)
+	if err != nil {
+		return nil, span.Error(err)
+	}
+
+	result := e.NewResult()
+	git := e.NewGit()
+
+	return &WerckerMessage{
+		Url:    e.applicationURL,
+		Build:  build,
+		Result: result,
+		Git:    git,
+	}, nil
+}
+
+func (e *WerckerEnv) Host() string {
+	return fmt.Sprintf("%s:%s", e.host, e.port)
 }
